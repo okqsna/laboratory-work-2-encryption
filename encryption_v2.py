97423a69f4ae83a4c05d20ab105b59c413db83d1
Anastasiia Yablunovska <yablunovska.pn@ucu.edu.ua>
2025-04-17 22:53:10 +0300

    minor change

--- encryption_v2.py
+++ encryption_v2.py
@@ -1,7 +1,7 @@
 """
 Another version of encryption to be fixed
 """
-
+# from rsa_algorithm import rsa_algorithm
 def encryption(word, n, e):
     """
     Encrypting the message
@@ -10,12 +10,13 @@ def encryption(word, n, e):
     :param n: int, public key
     :param e: int, e from rsa
     """
+    block_length = max_block_length(n)
     code = ''
     for i in word:
-        s = str(ord(i)).zfill(4)
+        s = str(ord(i)).zfill(block_length)
         code += s
 
-    block_length = max_block_length(n)
+
     blocks = [int(code[i:i+block_length]) for i in range(0, len(code), block_length)]
     for i, _ in enumerate(blocks):
         blocks[i] = pow(blocks[i], e, n)
@@ -30,14 +31,15 @@ def decrypt(blocks, d, n):
     :param d: int, private key
     :param n: int, public key
     """
+    block_length = max_block_length(n)
     lst = ''
     result = ''
     for block in blocks:
         m = pow(block, d, n)
-        lst += str(m).zfill(4)
+        lst += str(m).zfill(block_length)
 
-    for i in range(0, len(lst), 4):
-        result += chr(int(lst[i:i+4]))
+    for i in range(0, len(lst), block_length):
+        result += chr(int(lst[i:i+block_length]))
     return result
 
 def max_block_length(n):
@@ -54,3 +56,11 @@ def max_block_length(n):
 # block_decr = encryption('hello world', n_p, euler)
 # res = decrypt(block_decr, secret_key, n_p)
 # print(res)
+
+# dct = rsa_algorithm()
+# n = dct['n']
+# d = dct['secret_key_d']
+# e = dct['euler function']
+# word = 'hello 123 просто'
+# encr=encryption(word, n, e)
+# print(decrypt(encr, d, n))
